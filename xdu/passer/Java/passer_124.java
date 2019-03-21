16d32c9c467ee9f231f86d18d94d8866e9d57116
passer <1227118745@qq.com>
2019-03-21 20:50:29 +0800

    add sthe olution of leetcode problem s 114、124、136

---
+++ xdu/passer/Java/passer_124.java
@@ -0,0 +1,29 @@
+package com.passer._0321;
+
+class TreeNode {
+	int val;
+	TreeNode left;
+	TreeNode right;
+
+	TreeNode(int x) {
+		val = x;
+	}
+}
+
+public class MaxSumOfPath {
+    int maxValue;
+    
+    public int maxPathSum(TreeNode root) {
+        maxValue = Integer.MIN_VALUE;
+        maxPathDown(root);
+        return maxValue;
+    }
+    
+    private int maxPathDown(TreeNode node) {
+        if (node == null) return 0;
+        int left = Math.max(0, maxPathDown(node.left));
+        int right = Math.max(0, maxPathDown(node.right));
+        maxValue = Math.max(maxValue, left + right + node.val);
+        return Math.max(left, right) + node.val;
+    }
+}
